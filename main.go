a61d1e736ec4cceb8fe76d7d3e6cac1d4f8e67ea
Pablo Carranza <pcarranza@gmail.com>
2018-01-07 01:20:03 +0100

    Add graceful shutdown handling

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	log "github.com/sirupsen/logrus"
 	"gitlab.com/mr-meeseeks/meeseeks-box/auth"
@@ -28,36 +31,59 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	f, err := os.Open(*configFile)
-	if err != nil {
-		log.Fatalf("could not open configuration file %s: %s\n", *configFile, err)
-	}
-
-	cnf, err := config.New(f)
-	if err != nil {
-		fmt.Println(err)
-	}
+	cnf, err := loadConfiguration(*configFile)
+	must(err)
 
 	auth.Configure(cnf)
 
-	token := os.Getenv("SLACK_TOKEN")
-	if token == "" {
-		log.Fatalf("SLACK_TOKEN env var is empty")
-	}
+	client, err := slack.Connect(*debugMode)
+	must(err)
 
-	client, err := slack.New(slack.ClientConfig{
-		Token: token,
-		Debug: *debugMode,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
 	log.Println("Connected to slack")
 
 	meeseek := meeseeks.New(client, cnf)
-	ch := make(chan slack.Message)
-	go client.ListenMessages(ch)
-	for message := range ch {
-		meeseek.Process(message)
+
+	signalCh := make(chan os.Signal)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
+
+	messages := make(chan slack.Message)
+
+	go client.ListenMessages(messages)
+	wg := sync.WaitGroup{}
+
+processing:
+	for {
+		select {
+		case sig := <-signalCh:
+			log.Infof("Got signal %s, trying to gracefully shutdown", sig)
+			close(messages)
+			wg.Wait()
+			break processing
+
+		case message := <-messages:
+			go func(message slack.Message) {
+				wg.Add(1)
+				defer wg.Done()
+
+				meeseek.Process(message)
+			}(message)
+		}
+	}
+	log.Infof("All done, quitting")
+}
+
+func loadConfiguration(configFile string) (config.Config, error) {
+	f, err := os.Open(configFile)
+	if err != nil {
+		return config.Config{}, fmt.Errorf("could not open configuration file %s: %s", configFile, err)
+	}
+
+	return config.New(f)
+}
+
+func must(err error) {
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
 	}
 }
