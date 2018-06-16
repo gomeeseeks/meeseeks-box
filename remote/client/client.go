a4a39758d7a7e333f90f2ec50c469e25e0d7b188
Pablo Carranza <pcarranza@gmail.com>
2018-06-16 18:53:55 +0200

    WIP - client implementation

--- remote/client/client.go
+++ remote/client/client.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gomeeseeks/meeseeks-box/config"
 	"github.com/gomeeseeks/meeseeks-box/meeseeks"
+	"github.com/gomeeseeks/meeseeks-box/meeseeks/metrics"
 	"github.com/gomeeseeks/meeseeks-box/remote/api"
 
 	"github.com/sirupsen/logrus"
@@ -24,125 +25,29 @@ type Configuration struct {
 	Commands  map[string]config.Command
 }
 
-// New creates a new remote requester
-func New(c Configuration) *RemoteRequester {
-	return &RemoteRequester{
-		config:     c,
-		requestsCh: make(chan meeseeks.Request, c.Pool),
+func (c *Configuration) newContext() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), c.GetTimeout())
+}
+
+func (c *Configuration) createAgentConfiguration() *api.AgentConfiguration {
+	return &api.AgentConfiguration{
+		Commands: c.createRemoteCommands(),
+		Labels:   c.Labels,
+		Token:    c.Token,
 	}
 }
 
-// RemoteRequester is an implementation of an executor.Listener
-type RemoteRequester struct {
-	config     Configuration
-	grpcClient *grpc.ClientConn
-	requestsCh chan meeseeks.Request
-	stopCh     chan bool
+// GetTimeout returns the configured timeout or a default of 10 seconds
+func (c *Configuration) GetTimeout() time.Duration {
+	if c.Timeout == 0 {
+		return 10 * time.Second
+	}
+	return c.Timeout
 }
 
-// Listen implements Requester.Listen
-func (r *RemoteRequester) Listen(ch chan<- meeseeks.Request) {
-	for keepRunning := true; keepRunning; {
-		select {
-		case rq := <-r.requestsCh:
-			ch <- rq
-		case keepRunning = <-r.stopCh:
-			// do nothing
-		}
-	}
-}
-
-// Connect creates a connection to the remote server
-func (r *RemoteRequester) Connect() error {
-	c, err := grpc.Dial(r.config.ServerURL, r.config.Options)
-	if err != nil {
-		return fmt.Errorf("could not connect to remote server %s: %s", r.config.ServerURL, err)
-	}
-	r.grpcClient = c
-	return nil
-}
-
-// Start starts the client, listens to the remote channel and sends them to the execution pipeline
-func (r *RemoteRequester) Start() error {
-	if r.grpcClient == nil {
-		return fmt.Errorf("client is not connected to a remote server")
-	}
-
-	cmdClient := api.NewCommandPipelineClient(r.grpcClient)
-
-	ctx, cancel := r.config.newContext()
-	defer cancel()
-
-	// Register the commands and start listening on the stream of remote commands
-	// On startup it has to send all the commands that the meeseeks knows how to handle (except builtins)
-	stream, err := cmdClient.RegisterAgent(ctx, r.config.createAgentConfiguration())
-	if err != nil {
-		return fmt.Errorf("failed to register commands on remote server %s: %s", r.config.ServerURL, err)
-	}
-
-	// The whole logger thing should happen on connection by registering the logger for the shell commands
-	// // Get a logAppender
-	// logCtx, logCnl := this.config.newContext()
-	// defer logCnl()
-
-	// loggerClient := api.NewCommandLoggerClient(this.grpcClient)
-	// appender, err := loggerClient.NewAppender(logCtx)
-	// if err != nil {
-	// 	// Add metric for failing to get a log appender
-	// 	logrus.Fatalf("Could not open an appender stream: %s", err)
-	// }
-
-	// Get a command (or error)
-	for {
-		cmd, err := stream.Recv()
-		if err != nil {
-			logrus.Fatalf("failed to receive a command: %s", err)
-		}
-
-		// add a metric to account for remotely received commands
-		r.requestsCh <- meeseeks.Request{
-			Command:     cmd.Command,
-			Args:        cmd.Args,
-			Channel:     cmd.Channel,
-			ChannelID:   cmd.ChannelID,
-			ChannelLink: cmd.ChannelLink,
-			IsIM:        cmd.IsIM,
-			UserID:      cmd.UserID,
-			Username:    cmd.Username,
-			UserLink:    cmd.UserLink,
-		}
-	}
-
-	// for each command
-	// Execute the command and send all the logs
-	// for _, line := range []string{"line1", "line2", "line3", "line4"} {
-	// 	l := api.LogEntry{
-	// 		JobID: cmd.JobID,
-	// 		Line:  line}
-	// 	if err := appender.Send(&l); err != nil {
-	// 		logrus.Fatalf("Failed to send log line to backend: %s", err)
-	// 	}
-	// 	// add metric for sent log lines
-	// }
-	// if _, err := appender.CloseAndRecv(); err != nil {
-	// 	logrus.Fatalf("failed to close the channel: %s", err)
-	// }
-	// // Add metric for remotely finished commands
-	// _, err = commands.Finish(context.Background(), &api.CommandFinish{
-	// 	JobID:  cmd.JobID,
-	// 	Status: "Done",
-	// 	Error:  "",
-	// })
-	// if err != nil {
-	// 	logrus.Fatalf("Failed to finish the command execution %s", err)
-	// }
-
-	return nil
-}
-
-func (r Configuration) createRemoteCommands() map[string]*api.RemoteCommand {
-	remoteCommands := make(map[string]*api.RemoteCommand, len(r.Commands))
-	for name, cmd := range r.Commands {
+func (c *Configuration) createRemoteCommands() map[string]*api.RemoteCommand {
+	remoteCommands := make(map[string]*api.RemoteCommand, len(c.Commands))
+	for name, cmd := range c.Commands {
 		remoteCommands[name] = &api.RemoteCommand{
 			Timeout:         int64(cmd.Timeout),
 			AuthStrategy:    cmd.AuthStrategy,
@@ -159,87 +64,129 @@ func (r Configuration) createRemoteCommands() map[string]*api.RemoteCommand {
 	return remoteCommands
 }
 
-func (r Configuration) createAgentConfiguration() *api.AgentConfiguration {
-	return &api.AgentConfiguration{
-		Commands: r.createRemoteCommands(),
-		Labels:   r.Labels,
-		Token:    r.Token,
+// RemoteClient handles the configuration and the remote grpc client
+type RemoteClient struct {
+	config     Configuration
+	grpcClient *grpc.ClientConn
+}
+
+// New creates a new remote requester
+func New(c Configuration) *RemoteClient {
+	return &RemoteClient{
+		config: c,
+		// requestsCh: make(chan meeseeks.Request, c.Pool),
 	}
 }
 
-func (r Configuration) newContext() (context.Context, context.CancelFunc) {
-	return context.WithTimeout(context.Background(), r.GetTimeout())
-}
-
-// GetTimeout returns the configured timeout or a default of 10 seconds
-func (r Configuration) GetTimeout() time.Duration {
-	if r.Timeout == 0 {
-		return 10 * time.Second
+// Connect creates a connection to the remote server
+func (r *RemoteClient) Connect() error {
+	c, err := grpc.Dial(r.config.ServerURL, r.config.Options)
+	if err != nil {
+		return fmt.Errorf("could not connect to remote server %s: %s", r.config.ServerURL, err)
 	}
-	return r.Timeout
+	r.grpcClient = c
+	return nil
 }
 
-// LoggerProvider wraps the specific logger implementation
-// type LoggerProvider interface {
-// 	Reader(jobID uint64) LogReader
-// 	Writer(jobID uint64) LogWriter
-// }
-type grpcLoggerProvider struct {
-	conn *grpc.ClientConn
-}
-
-func newLoggerProvider(conn *grpc.ClientConn) grpcLoggerProvider {
-	return grpcLoggerProvider{
-		conn: conn,
+// Requester creates a new requester using the client
+func (r *RemoteClient) Requester() *RemoteRequester {
+	return &RemoteRequester{
+		client:     r,
+		requestsCh: make(chan meeseeks.Request, r.config.Pool),
 	}
 }
 
-// Reader implements
-func (g grpcLoggerProvider) Reader() meeseeks.LogReader {
+// LogWriter creates a new LogWriter
+func (r *RemoteClient) LogWriter() meeseeks.LogWriter {
+	return grpcLogWriter{
+		remoteClient: r,
+	}
+}
+
+// LogReader returns a new remote log reader
+func (r *RemoteClient) LogReader() meeseeks.LogReader {
 	return nullReader{}
 }
 
-type nullReader struct {
-	jobID uint64
+// RemoteRequester is an implementation of an executor.Listener
+type RemoteRequester struct {
+	client     *RemoteClient
+	requestsCh chan meeseeks.Request
+	stopCh     chan bool
 }
 
-// Get implements meeseeks.LogReader.Get
-func (n nullReader) Get(_ uint64) (meeseeks.JobLog, error) {
-	return meeseeks.JobLog{}, nil
+// Listen implements Requester.Listen
+func (r *RemoteRequester) Listen(ch chan<- meeseeks.Request) {
+	for keepRunning := true; keepRunning; {
+		select {
+		case rq := <-r.requestsCh:
+			ch <- rq
+		case keepRunning = <-r.stopCh:
+			// do nothing
+		}
+	}
 }
 
-// Head implements meeseeks.LogReader.Head
-func (n nullReader) Head(_ uint64, _ int) (meeseeks.JobLog, error) {
-	return meeseeks.JobLog{}, nil
-}
+// Start starts the client, listens to the remote channel and sends them to the execution pipeline
+func (r *RemoteRequester) Start() error {
+	if r.client.grpcClient == nil {
+		return fmt.Errorf("client is not connected to a remote server")
+	}
 
-// Tail implements meeseeks.LogReader.Tail
-func (n nullReader) Tail(_ uint64, _ int) (meeseeks.JobLog, error) {
-	return meeseeks.JobLog{}, nil
-}
+	cmdClient := api.NewCommandPipelineClient(r.client.grpcClient)
 
-// Writer returns a LogWriter
-func (g grpcLoggerProvider) Writer() meeseeks.LogWriter {
-	client := api.NewLogWriterClient(g.conn)
-	appender, err := client.NewWriter(context.Background()) // TODO improve this with some options
+	ctx, cancel := r.client.config.newContext()
+	defer cancel()
+
+	// Register the commands and start listening on the stream of remote commands
+	// On startup it has to send all the commands that the meeseeks knows how to handle (except builtins)
+	commandStream, err := cmdClient.RegisterAgent(ctx, r.client.config.createAgentConfiguration())
 	if err != nil {
-		logrus.Fatalf("could not create appender: %s", err)
+		return fmt.Errorf("failed to register commands on remote server %s: %s", r.client.config.ServerURL, err)
 	}
 
-	return grpcLogWriter{
-		client:   client,
-		appender: appender,
+	// Get a command (or error)
+	for {
+		cmd, err := commandStream.Recv()
+		if err != nil {
+			logrus.Fatalf("failed to receive command: %s", err)
+			break
+		}
+
+		// add a metric to account for remotely received commands
+		r.requestsCh <- meeseeks.Request{
+			Command:     cmd.Command,
+			Args:        cmd.Args,
+			Channel:     cmd.Channel,
+			ChannelID:   cmd.ChannelID,
+			ChannelLink: cmd.ChannelLink,
+			IsIM:        cmd.IsIM,
+			UserID:      cmd.UserID,
+			Username:    cmd.Username,
+			UserLink:    cmd.UserLink,
+		}
 	}
+
+	return nil
 }
 
 type grpcLogWriter struct {
-	client   api.LogWriterClient
-	appender api.LogWriter_NewWriterClient
+	remoteClient *RemoteClient
 }
 
 // Append implements LogWritter.Append
 func (g grpcLogWriter) Append(jobID uint64, content string) error {
-	return g.appender.Send(&api.LogEntry{
+	ctx, cancel := g.remoteClient.config.newContext()
+	defer cancel()
+
+	c := api.NewLogWriterClient(g.remoteClient.grpcClient)
+	w, e := c.NewWriter(ctx)
+	if e != nil {
+		return e
+	}
+
+	metrics.LogLinesCount.Inc()
+	return w.Send(&api.LogEntry{
 		JobID: jobID,
 		Line:  content,
 	})
@@ -247,9 +194,28 @@ func (g grpcLogWriter) Append(jobID uint64, content string) error {
 
 // SetError implements LogWritter.SetError
 func (g grpcLogWriter) SetError(jobID uint64, jobErr error) error {
-	_, err := g.client.SetError(context.Background(), &api.ErrorLogEntry{
+	ctx, cancel := g.remoteClient.config.newContext()
+	defer cancel()
+
+	c := api.NewLogWriterClient(g.remoteClient.grpcClient)
+	_, err := c.SetError(ctx, &api.ErrorLogEntry{
 		JobID: jobID,
 		Error: jobErr.Error(),
 	})
 	return err
+}
+
+type nullReader struct {
+}
+
+func (nullReader) Get(_ uint64) (meeseeks.JobLog, error) {
+	return meeseeks.JobLog{}, nil
+}
+
+func (nullReader) Head(_ uint64, _ int) (meeseeks.JobLog, error) {
+	return meeseeks.JobLog{}, nil
+}
+
+func (nullReader) Tail(_ uint64, _ int) (meeseeks.JobLog, error) {
+	return meeseeks.JobLog{}, nil
 }
